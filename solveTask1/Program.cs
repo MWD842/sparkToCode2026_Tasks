7074591c1688364921c2773c15708a04c8251aa4
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-07 19:48:42 -0700

    add product files

---
+++ solveTask1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace solveTask1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
