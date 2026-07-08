a8eec3be1079a1616f6c02dca2b1e917bba66dfc
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-07 19:50:31 -0700

    Personal Info Card

--- solveTask1/Program.cs
+++ solveTask1/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // TASK 1: Write a C# program that takes user input for their name, age, height, and whether they are a student or not. Then, display the information back to the user in a formatted string.
+            Console.WriteLine("Enter your name:");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Enter your age:");
+            int userAge = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter your height:");
+            float userHeight = float.Parse(Console.ReadLine());
+            Console.WriteLine("Are you a student? (Trure/False):");
+            string isStudent = Console.ReadLine();
+            Console.WriteLine($"Name: {userName}, Age: {userAge}, " +
+                $"Height: {userHeight}, Student: {isStudent}"); 
         }
     }
 }
