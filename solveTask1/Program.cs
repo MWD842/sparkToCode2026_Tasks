a41943aecbb7049978ed11b03465c39d12da25bd
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-07 19:52:25 -0700

    Even or Odd Checker

--- solveTask1/Program.cs
+++ solveTask1/Program.cs
@@ -17,18 +17,28 @@
             //    $"Height: {userHeight}, Student: {isStudent}"); 
 
             // TASK 2: Write a C# program that takes user input for two numbers and performs basic arithmetic operations (addition, subtraction, multiplication, division) on them. Display the results to the user.
-            Console.WriteLine("Enter the length: ");
-            double length = double.Parse(Console.ReadLine());
-            Console.WriteLine("Enter the width: ");
-            double width = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the length: ");
+            //double length = double.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter the width: ");
+            //double width = double.Parse(Console.ReadLine());
 
-            double area = length * width;
-            double perimeter = 2 * (length + width);
+            //double area = length * width;
+            //double perimeter = 2 * (length + width);
 
-            Console.WriteLine("Area: " + area);
-            Console.WriteLine("Perimeter: " + perimeter);
+            //Console.WriteLine("Area: " + area);
+            //Console.WriteLine("Perimeter: " + perimeter);
 
-
+            // TASK 3: Write a C# program that takes user input for a number and checks if it is even or odd. Display an appropriate message based on the result.
+            Console.WriteLine("Enter the a number: "); 
+            int number = int.Parse(Console.ReadLine());
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("The number is even.");
+            }
+            else
+            {
+                Console.WriteLine("The number is odd.");
+            }
         }
     }
 }
