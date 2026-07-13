377bd7caee3f2bcca4d541596cf2300a88a0cf7f
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 11:01:24 +0400

    Task 3 - Celsius to Fahrenheit Function

--- solveTask4/Program.cs
+++ solveTask4/Program.cs
@@ -15,6 +15,14 @@
             int result = number * number;
             return result;
         }
+
+        // TASK 3
+        public static double CelsiusToFahrenheit(double C)
+        {
+            double F = (C * 9 / 5) + 32;
+            return F;
+        }
+
         static void Main(string[] args)
         {
             // TASK 1
@@ -25,13 +33,18 @@
             /////////
 
             // TASK 2
-            Console.WriteLine("Enter a number to square: ");
-            int number = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter a number to square: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
 
-            int squaredNumber = Square(number);
-            Console.WriteLine("The square of " + number + " is: " + squaredNumber);
+            //int squaredNumber = Square(number);
+            //Console.WriteLine("The square of " + number + " is: " + squaredNumber);
 
+            /////////
 
+            // TASK 3
+            Console.WriteLine("Enter a temperature in Celsius: ");
+            double C = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("The temperature in Fahrenheit is: " + CelsiusToFahrenheit(C));
         }
     }
 }
