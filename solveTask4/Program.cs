daa0609043628f2d533a6dfc0305a4c4cb87840f
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 10:50:22 +0400

    add project files

---
+++ solveTask4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace solveTask4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
