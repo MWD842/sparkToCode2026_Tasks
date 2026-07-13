17fd6bafa9c0cf06629d18923b7d43a449d0e153
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 09:56:27 +0400

    Task 1 - Countdown Timer

--- solveTask2/Program.cs
+++ solveTask2/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //TASK 1: Write a program that counts down from a given number to zero and then prints "Liftoff!".
+            Console.WriteLine("Enter a starting number: ");
+            int start = int.Parse(Console.ReadLine());
+            for (int i = start; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Liftoff!");
         }
     }
 }
