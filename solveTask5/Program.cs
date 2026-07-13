9cd15d547dbb2f39d5494a413c0bae49f3902c20
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-13 07:03:36 -0700

    Task 1 - Fixed Grades Array

--- solveTask5/Program.cs
+++ solveTask5/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // TASK 1
+            int[] grades = new int[5];
+
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine($"Enter grade grades{i+1}: ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("\nGrades entered: ");
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
         }
     }
 }
