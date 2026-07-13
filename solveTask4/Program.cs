0d3a7e185e863377ebc1ab6dd9a1db5e896cb6cd
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 10:54:26 +0400

    Task 2 - Square Number Function

--- solveTask4/Program.cs
+++ solveTask4/Program.cs
@@ -8,15 +8,29 @@
 
             Console.WriteLine($"Welcome to Spark to Code, {userName}");
         }
+
+        // TASK 2
+        public static int Square(int number)
+        {
+            int result = number * number;
+            return result;
+        }
         static void Main(string[] args)
         {
             // TASK 1
-            Console.WriteLine("Enter your name: ");
-            string userName = Console.ReadLine();
-            PrintWelcome(userName);
+            //Console.WriteLine("Enter your name: ");
+            //string userName = Console.ReadLine();
+            //PrintWelcome(userName);
 
             /////////
-            
+
+            // TASK 2
+            Console.WriteLine("Enter a number to square: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            int squaredNumber = Square(number);
+            Console.WriteLine("The square of " + number + " is: " + squaredNumber);
+
 
         }
     }
