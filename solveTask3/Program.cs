40bd82901ac1090a5fd405aa6b466f5102ab53a0
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 10:29:10 +0400

    Task 1 - Absolute Difference

--- solveTask3/Program.cs
+++ solveTask3/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // Task 1: Write a program that takes two integers as input and prints the absolute difference between them.
+            Console.WriteLine("Enter the first number: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the second number: ");
+            int num2 = int.Parse(Console.ReadLine());
+            int result = Math.Abs(num1 - num2);
+            Console.WriteLine("The absolute difference is: " + result);
+
+            ////////
         }
     }
 }
