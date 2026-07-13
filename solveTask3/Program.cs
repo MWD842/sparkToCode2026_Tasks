fa178df9859304f23eecd9e8848e5481b06a4938
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 10:33:25 +0400

    Task 4 - Subscription End Date

--- solveTask3/Program.cs
+++ solveTask3/Program.cs
@@ -25,13 +25,21 @@
             ////////
 
             // Task 3: Write a program that takes a string as input and prints the string in uppercase, lowercase, and the length of the string.
-            Console.WriteLine("Enter your name: ");
-            string upper = Console.ReadLine().ToUpper();
-            string lower = upper.ToLower();
-            string length = upper.Length.ToString();
-            Console.WriteLine("Your name in uppercase is: " + upper);
-            Console.WriteLine("Your name in lowercase is: " + lower);
-            Console.WriteLine("The length of your name is: " + length);
+            //Console.WriteLine("Enter your name: ");
+            //string upper = Console.ReadLine().ToUpper();
+            //string lower = upper.ToLower();
+            //string length = upper.Length.ToString();
+            //Console.WriteLine("Your name in uppercase is: " + upper);
+            //Console.WriteLine("Your name in lowercase is: " + lower);
+            //Console.WriteLine("The length of your name is: " + length);
+
+            ////////
+
+            // Task 4: Write a program that takes a positive whole number as input and prints the multiplication table for that number up to 10.
+            Console.WriteLine("Enter the number of days of a free trial: ");
+            int days = int.Parse(Console.ReadLine());
+            DateTime today = DateTime.Today.AddDays(days);
+            Console.WriteLine("The free trial will end on: " + today.ToString("D"));
 
             ////////
         }
