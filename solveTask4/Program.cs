d41e3b758a3edf2843d383b331039816b5d0773d
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 10:52:19 +0400

    Task 1 - Personalized Welcome Function

--- solveTask4/Program.cs
+++ solveTask4/Program.cs
@@ -2,9 +2,22 @@
 {
     internal class Program
     {
+        // TASK 1
+        public static void PrintWelcome(string userName)
+        {
+
+            Console.WriteLine($"Welcome to Spark to Code, {userName}");
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // TASK 1
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+            PrintWelcome(userName);
+
+            /////////
+            
+
         }
     }
 }
