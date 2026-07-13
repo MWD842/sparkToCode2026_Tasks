60a3eca55615f09dcf105780fdc5ff808d94e05e
mowadah al waili <MWD@DESKTOP-IBEP3PJ>
2026-07-13 11:03:13 +0400

    Task 4 - Fixed Menu Display Function

--- solveTask4/Program.cs
+++ solveTask4/Program.cs
@@ -23,6 +23,37 @@
             return F;
         }
 
+        // TASK 4
+        public static void DisplayMenu()
+        {
+            int choice = 0;
+            do
+            {
+                Console.WriteLine("Menu\n1) Start\n2) Help\n3) Exit");
+                Console.WriteLine("Enter your choice (1-3): ");
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("\nStart\n");
+                            break;
+                        case 2:
+                            Console.WriteLine("\nHelp\n");
+                            break;
+                        case 3:
+                            Console.WriteLine("\nExit\n");
+                            return;
+                    }
+
+                }
+                catch
+                {
+                    Console.WriteLine("\nInvlid input!!\n");
+                }
+            } while (true);
+        }
         static void Main(string[] args)
         {
             // TASK 1
@@ -42,9 +73,14 @@
             /////////
 
             // TASK 3
-            Console.WriteLine("Enter a temperature in Celsius: ");
-            double C = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("The temperature in Fahrenheit is: " + CelsiusToFahrenheit(C));
+            //Console.WriteLine("Enter a temperature in Celsius: ");
+            //double C = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("The temperature in Fahrenheit is: " + CelsiusToFahrenheit(C));
+
+            /////////
+
+            // TASK 4
+            DisplayMenu();
         }
     }
 }
