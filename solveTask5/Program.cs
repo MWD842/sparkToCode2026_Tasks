97d10bd9a862e1a8a53be2b494936cba1670cf3a
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-13 07:32:49 -0700

    Task 2 - Dynamic To-Do List

--- solveTask5/Program.cs
+++ solveTask5/Program.cs
@@ -5,18 +5,38 @@
         static void Main(string[] args)
         {
             // TASK 1
-            int[] grades = new int[5];
+            //int[] grades = new int[5];
 
-            for (int i = 0; i < grades.Length; i++)
+            //for (int i = 0; i < grades.Length; i++)
+            //{
+            //    Console.WriteLine($"Enter grade grades{i+1}: ");
+            //    grades[i] = int.Parse(Console.ReadLine());
+            //}
+
+            //Console.WriteLine("\nGrades entered: ");
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //////////
+            
+            // TASK 2
+            List<string> tasks = new List<string>();
+
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine($"Enter grade grades{i+1}: ");
-                grades[i] = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Enter task {i+1}");
+                string task = Console.ReadLine();
+                tasks.Add( task );
             }
 
-            Console.WriteLine("\nGrades entered: ");
-            foreach (int grade in grades)
+            Console.WriteLine("\nYour to-do list: ");
+            int number = 1;
+            foreach (string task in tasks)
             {
-                Console.WriteLine(grade);
+                Console.WriteLine($"{number}. {task}");
+                number++;
             }
         }
     }
