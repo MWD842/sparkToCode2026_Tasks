7c5942925919154e61bd5a077495e2c4c9cb9ad1
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-14 22:17:13 -0700

    add accounts, students, and products

--- OOP_Part1_TaskSolution/Program.cs
+++ OOP_Part1_TaskSolution/Program.cs
@@ -107,6 +107,14 @@ namespace OOP_Part1_TaskSolution
 
     public class Program
     {
+        static BankAccount account1 = new BankAccount { AccountNumber = 20213333, HolderName = "Mowadah", Balance = 120 };
+        static BankAccount account2 = new BankAccount { AccountNumber = 20224573, HolderName = "Rahma", Balance = 100 };
+
+        static Student student1 = new Student { Name = "Reem", Address = "Muscat", Grade= 98 };
+        static Student student2 = new Student { Name = "Sara", Address = "Ibri", Grade = 96 };
+
+        static Product product1 = new Product { ProductName = "Wireless Mouse", Price = 5.500, StockQuantity = 50 };
+        static Product product2 = new Product { ProductName = "Mechanical Keyboard", Price = 15.750, StockQuantity = 20 };
 
     }
 }
