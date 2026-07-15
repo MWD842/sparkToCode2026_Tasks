cdd5330a0a907ce7a0864cdd66e8d0b5e218fa4a
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-14 21:29:03 -0700

    class Student is ready

--- OOP_Part1_TaskSolution/Program.cs
+++ OOP_Part1_TaskSolution/Program.cs
@@ -37,12 +37,28 @@ namespace OOP_Part1_TaskSolution
 
         void SendEmail()
         {
-            Console.WriteLine("Email notification sent")
+            Console.WriteLine("Email notification sent");
         }
     }
 
     public class Student
     {
+        public int Grade { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        private string email {  get; set; }
+        int age { get; set; }
+
+        public void Register(string Email)
+        {
+            email = Email;
+            SendEmail();
+        }
+
+        private void SendEmail()
+        {
+            Console.WriteLine("Email notification sent");
+        }
 
     }
 
