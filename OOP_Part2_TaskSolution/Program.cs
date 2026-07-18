51bbc2781d8d18cbfbec725d14966312519f58f2
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-07-18 13:16:09 -0700

    class Guest

--- OOP_Part2_TaskSolution/Program.cs
+++ OOP_Part2_TaskSolution/Program.cs
@@ -21,4 +21,32 @@
             Console.WriteLine($"Room {roomNumber} | {roomType} | {pricePerNight} OMR | {status}");
         }
     }
+
+    public class Guest
+    {
+        public string guestId;
+        public string guestName;
+        public string roomNumber;
+        public string checkInDate;
+        public int totalNights;
+
+        public Guest(string guestId, string guestName, string checkInDate, int totalNights)
+        {
+            this.guestId = guestId;
+            this.guestName = guestName;
+            this.roomNumber = "Not Assigned";
+            this.checkInDate = checkInDate;
+            this.totalNights = totalNights;
+        }
+
+        public void displayGuest()
+        {
+            Console.WriteLine($"{guestId} | {guestName} | Room {roomNumber} | {checkInDate} | {totalNights} nights");
+        }
+
+        public double calculateTotalCost(double pricePerNight)
+        {
+            return pricePerNight * totalNights;
+        }
+    }
 }
