8252cf489eec914b337b26495d763e121556b3bd
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-08-06 10:24:15 -0700

    Add Models & their data

---
+++ WebAPIProject/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAPIProject.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string CategoryDescription { get; set; }
+
+        // Relationship
+        public List<Product> Products { get; set; }
+    }
+}
