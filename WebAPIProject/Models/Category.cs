ce2273ed1ff7885d12589765e9d1f123d9f464c0
Mowadah Khalifa Al Waili <maalwaili444@gmail.com>
2026-08-06 15:05:10 -0700

    Add [JsonIgnore]

--- WebAPIProject/Models/Category.cs
+++ WebAPIProject/Models/Category.cs
@@ -1,15 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace WebAPIProject.Models
 {
     public class Category
     {
         [Key]
+        [JsonIgnore]
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
 
         // Relationship
+        [JsonIgnore]
         public List<Product> Products { get; set; }
     }
 }
--- WebAPIProject/Models/Product.cs
+++ WebAPIProject/Models/Product.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization;
 
 namespace WebAPIProject.Models
 {
     public class Product
     {
         [Key]
+        [JsonIgnore]
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductDescription { get; set; }
@@ -14,6 +16,8 @@ namespace WebAPIProject.Models
         //Relationship
         [ForeignKey("_category")]
         public int CategoryId { get; set; }
+
+        [JsonIgnore]
         public Category _category { get; set; }
 
     }
